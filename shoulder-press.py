6ea5d372968d56c710bb76c57773b605485f4454
addinar <raddina24@gmail.com>
2025-05-03 03:01:41 -0400

    added shoulder press model

---
+++ shoulder-press.py
@@ -0,0 +1,61 @@
+from ultralytics import solutions
+import cv2
+#import streamlit as st
+from collections import deque
+import subprocess
+import os
+
+def shoulder_press(path):
+    cap = cv2.VideoCapture(path)
+    assert cap.isOpened(), "Error reading video file"
+    w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+    cwd = os.getcwd()
+
+    output_avi = os.path.join(cwd, "Shoulderpress.demo.video.output.avi")
+    output_mp4 = os.path.join(cwd, "Shoulderpress.demo.video.output.mp4")
+
+    video_writer = cv2.VideoWriter(output_avi, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+    gym = solutions.AIGym(
+        show=True,
+        kpts=[5, 7, 9],
+        model="yolo11n-pose.pt",  # Path to the YOLO11 pose estimation model file
+        line_width=4,  # Adjust the line width for bounding boxes and text display
+        up_angle=127,
+        down_angle=95,
+        verbose=False,
+    )
+
+    queue = deque(maxlen=fps) # contains the last state
+    while cap.isOpened():
+        success, im0 = cap.read()
+        if not success:
+            # st.toast('Video frame is empty or video processing has been successfully completed.')
+            print('Video frame is empty or video processing has been successfully completed.')
+            break
+            # uncomment st.toast when streamlit is successfully implemented
+        results = gym(im0)
+        curr_stage = results.workout_stage[0]
+        queue.append(curr_stage)
+        if len(queue) == queue.maxlen and len(set(queue)) == 1:
+            # st.toast("Check your form.")
+            print('Check your form.')
+            # uncomment st.toast when streamlit is successfully implemented
+        video_writer.write(results.plot_im)
+
+        #if st.button('Stop'):
+            #break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    video_writer.release()
+
+    subprocess.run([
+        'ffmpeg', '-i', output_avi,
+        '-c:v', 'libx264',
+        '-preset', 'medium',
+        '-crf', '23',
+        output_mp4
+    ])
+
+shoulder_press('RPReplay_Final1746250197.mov')
